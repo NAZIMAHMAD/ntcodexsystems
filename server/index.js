efd91d7509877c1fc0fcc5d02dc0e0fb794dd84b
nazim ahmad <zk8052272@gmail.com>
2024-12-31 21:23:51 +0530

    api updated

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const fs = require('fs'); // File System module to read files
 const app = express();
-const port = 3000
 
-app.get('/' ,(req, res) => {
+const port = 3000;
 
-res.send('i am ok now , server47 is running');
-
-})
+// Route that serves the JSON data from the user.json file
+app.get('/user', (req, res) => {
+  // Read the user.json file asynchronously
+  fs.readFile('user.json', (err, data) => {
+   
+      // Parse the JSON data and send it as the response
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+      }
+);
+});
 
 app.listen(port, () => {
-
-console.log('port is running ');
-
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
